d22f8ec01f626abf2577ce2cc30f4c19064161a6
Ionel Cristian Mărieș <contact@ionelmc.ro>
2024-04-11 19:28:20 +0300

    Skip osx.

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,9 +1,11 @@
 import subprocess
+import sys
 from sysconfig import get_python_version
 
 import pytest
 
 
+@pytest.mark.skipif(sys.platform != "darwin", reason="no Docker")
 @pytest.mark.parametrize("dockerfile", ["tests/test-alpine.Dockerfile", "tests/test-debian.Dockerfile"])
 def test_main(dockerfile):
     subprocess.check_call(["docker", "build", ".", "-f", dockerfile, "--build-arg", f"PYTHON={get_python_version()}"])
